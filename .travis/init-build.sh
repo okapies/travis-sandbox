27689404e8e9b6ef8635ffeba9232e699895fdae
Yuta Okamoto <okapies@gmail.com>
2018-09-10 16:34:08 +0900

    add mkl-dnn

--- .travis/init-build.sh
+++ .travis/init-build.sh
@@ -1,6 +1,33 @@
-DOCKER_CONTAINER_ID=$1
+# check if variables have values
+test -n "${DOCKER_CONTAINER_ID}" || { echo "DOCKER_CONTAINER_ID does not exist"; exit 1; }
+test -n "${MKLDNN_VERSION}" || { echo "MKLDNN_VERSION does not exist"; exit 1; }
+test -n "${GCC_ROOT_DIR}" || { echo "GCC_ROOT_DIR does not exist"; exit 1; }
+test -n "${MKLDNN_INSTALL_DIR}" || { echo "MKLDNN_INSTALL_DIR does not exist"; exit 1; }
 
 function docker_exec() {
-	local cmd=$1
-    docker exec -it ${DOCKER_CONTAINER_ID} /bin/bash -xec "$cmd"
+    docker exec -it ${DOCKER_CONTAINER_ID} /bin/bash -xec "$1"
+}
+
+function build_mkldnn() {
+    docker_exec "$(cat << EOS
+cd ${HOME}/build && \
+wget https://github.com/intel/mkl-dnn/archive/v${MKLDNN_VERSION}.tar.gz && \
+tar -zxf v${MKLDNN_VERSION}.tar.gz
+EOS
+)"
+    docker_exec "cd ${HOME}/build/mkl-dnn-${MKLDNN_VERSION}/scripts && ./prepare_mkl.sh"
+    docker_exec "$(cat << EOS
+cd ${HOME}/build/mkl-dnn-${MKLDNN_VERSION} && mkdir -p build && cd build && \
+cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_C_COMPILER=${GCC_ROOT_DIR}/bin/gcc \
+  -DCMAKE_CXX_COMPILER=${GCC_ROOT_DIR}/bin/g++ \
+  -DCMAKE_INSTALL_PREFIX=${MKLDNN_INSTALL_DIR} \
+  -DWITH_TEST=OFF \
+  -DWITH_EXAMPLE=OFF \
+  -Wno-error=unused-result \
+  ..
+EOS
+)"
+    docker_exec "cd ${HOME}/build/mkl-dnn-${MKLDNN_VERSION}/build && make -j$(nproc) && make install"
 }
--- .travis/linux-x86_64/build.sh
+++ .travis/linux-x86_64/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -ex
 
 BASE_DIR=$(cd $(dirname $0) && pwd)
+
+GCC_ROOT_DIR=/usr/local/gcc-${GCC_VERSION}
+MKLDNN_INSTALL_DIR=${HOME}/mkl-dnn-${MKLDNN_VERSION}
+
 source ${BASE_DIR}/../init-build.sh
 
 docker_exec "ls -l ${HOME}"
@@ -8,3 +12,13 @@ docker_exec "ls -l ${HOME}/build"
 docker_exec "ls -l ${HOME}/build/${TRAVIS_REPO_SLUG}"
 
 docker_exec "echo hello > ${HOME}/build/result.txt"
+
+docker_exec "make --version && cmake --version && ${GCC_ROOT_DIR}/bin/g++ --version && ldd --version"
+
+# build MKL-DNN if it doesn't exist
+[ ! -e "${MKLDNN_INSTALL_DIR}/lib/libmkldnn.so" ] && build_mkldnn
+
+ls -l ${HOME}/build/mkl-dnn-${MKLDNN_VERSION}/build
+ls -l ${HOME}/mkl-dnn-${MKLDNN_VERSION}
+
+docker_exec "false"
