96a98e7c0fdfff892aaa5fc0c16876b4bc060fd9
Yuta Okamoto <okapies@gmail.com>
2018-09-11 12:47:43 +0900

    Revert "use centos:centos7"
    This reverts commit cc3d92249dff7979f37c0e6cfa62a33d56438521.

--- .travis/linux-x86_64/build.sh
+++ .travis/linux-x86_64/build.sh
@@ -2,7 +2,7 @@
 
 BASE_DIR=$(cd $(dirname $0) && pwd)
 
-GCC_ROOT_DIR=/usr
+GCC_ROOT_DIR=/usr/local/gcc-${GCC_VERSION}
 PROTOBUF_INSTALL_DIR=${HOME}/protobuf-${PROTOBUF_VERSION}
 MKLDNN_INSTALL_DIR=${HOME}/mkl-dnn-${MKLDNN_VERSION}
 
@@ -14,7 +14,7 @@ docker_exec "ls -l ${HOME}/build/${TRAVIS_REPO_SLUG}"
 
 docker_exec "echo hello > ${HOME}/build/result.txt"
 
-#docker_exec "make --version && cmake --version && ${GCC_ROOT_DIR}/bin/g++ --version && ldd --version"
+docker_exec "make --version && cmake --version && ${GCC_ROOT_DIR}/bin/g++ --version && ldd --version"
 
 # build dependencies if it doesn't exist
 [ ! -e "${MKLDNN_INSTALL_DIR}/lib/libmkldnn.so" ] && build_mkldnn
